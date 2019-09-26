69eb568d587d174d2d7d1ad604976523337183b4
Bryan Matthews <starfishbam@gmail.com>
2019-09-25 20:58:23 -0700

    Minor updates

--- scripts/updateProject.bat
+++ scripts/updateProject.bat
@@ -8,12 +8,12 @@ echo Running tcl script to regenerate project
 Rem Delete generated files and folders
 echo Deleting generated files and folders
 
-RMDIR /S /Q "../bd/zyboZ7/ip"
-RMDIR /S /Q "../bd/zyboZ7/ipshared"
-RMDIR /S /Q "../bd/zyboZ7/sim"
-RMDIR /S /Q "../bd/zyboZ7/synth"
-RMDIR /S /Q "../bd/zyboZ7/ui"
-DEL /Q "..\bd\zyboZ7\zyboZ7.bxml"
-DEL /Q "..\bd\zyboZ7\zyboZ7_ooc.xdc"
+RMDIR /S /Q "../bd/ZyboZ7Bd/ip"
+RMDIR /S /Q "../bd/ZyboZ7Bd/ipshared"
+RMDIR /S /Q "../bd/ZyboZ7Bd/sim"
+RMDIR /S /Q "../bd/ZyboZ7Bd/synth"
+RMDIR /S /Q "../bd/ZyboZ7Bd/ui"
+DEL /Q "..\bd\ZyboZ7Bd\ZyboZ7Bd.bxml"
+DEL /Q "..\bd\ZyboZ7Bd\ZyboZ7Bd_ooc.xdc"
 
 PAUSE

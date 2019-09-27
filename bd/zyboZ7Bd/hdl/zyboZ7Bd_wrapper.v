02aecf70da48e86d307f4e7d1b593b2dd2592b32
Bryan Matthews <starfishbam@gmail.com>
2019-09-26 20:50:56 -0700

    Use correct top level wrapper

--- bd/zyboZ7Bd/hdl/zyboZ7Bd_wrapper.v
+++ bd/zyboZ7Bd/hdl/zyboZ7Bd_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-//Date        : Wed Sep 25 21:00:31 2019
+//Date        : Thu Sep 26 20:49:53 2019
 //Host        : DESKTOP-8NLVAFE running 64-bit major release  (build 9200)
 //Command     : generate_target zyboZ7Bd_wrapper.bd
 //Design      : zyboZ7Bd_wrapper
